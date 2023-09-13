080c68b2c0cb133e2967ab6379c768c04285a07d
surajac1 <surajac1996@gmail.com>
2023-09-13 16:17:17 +0530

    no changes

--- projetStash/src/main/java/com/cjc/controller/HomeController.java
+++ projetStash/src/main/java/com/cjc/controller/HomeController.java
@@ -1,27 +1,14 @@
 package com.cjc.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.cjc.model.User;
-import com.cjc.service.HomeService;
 
 @RestController
 public class HomeController {
+	
+	
+	
+	
+	
+	
 
-@Autowired
-HomeService hs;
-	
-	@GetMapping("/getall")
-public ResponseEntity<List<User>> getall(@RequestBody User user) {
-		List<User> userlist=hs.getAlluser();
-		return new ResponseEntity<List<User>>(userlist, HttpStatus.OK);
-	}
-	
 }
